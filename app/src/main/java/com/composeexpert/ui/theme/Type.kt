4a2ce11ae941cd3831acababfe75b654873343ed
pablo galvan <gvpablodaniel@gmail.com>
2024-10-26 01:08:11 -0300

    - Disable dynamicColor - Change FontFamily: Monospace

--- app/src/main/java/com/composeexpert/ui/theme/Type.kt
+++ app/src/main/java/com/composeexpert/ui/theme/Type.kt
@@ -9,26 +9,27 @@ import androidx.compose.ui.unit.sp
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+).defaultFontFamily(fontFamily = FontFamily.Monospace)
+
+fun Typography.defaultFontFamily(fontFamily: FontFamily) = this.copy(
+    displayLarge = this.displayLarge.copy(fontFamily = fontFamily),
+    displayMedium = this.displayMedium.copy(fontFamily = fontFamily),
+    displaySmall = this.displaySmall.copy(fontFamily = fontFamily),
+    headlineLarge = this.headlineLarge.copy(fontFamily = fontFamily),
+    headlineMedium = this.headlineMedium.copy(fontFamily = fontFamily),
+    headlineSmall = this.headlineSmall.copy(fontFamily = fontFamily),
+    titleLarge = this.titleLarge.copy(fontFamily = fontFamily),
+    titleMedium = this.titleMedium.copy(fontFamily = fontFamily),
+    titleSmall = this.titleSmall.copy(fontFamily = fontFamily),
+    bodyLarge = this.bodyLarge.copy(fontFamily = fontFamily),
+    bodyMedium = this.bodyMedium.copy(fontFamily = fontFamily),
+    bodySmall = this.bodySmall.copy(fontFamily = fontFamily),
+    labelLarge = this.labelLarge.copy(fontFamily = fontFamily),
+    labelMedium = this.labelMedium.copy(fontFamily = fontFamily),
+    labelSmall = this.labelSmall.copy(fontFamily = fontFamily)
 )
