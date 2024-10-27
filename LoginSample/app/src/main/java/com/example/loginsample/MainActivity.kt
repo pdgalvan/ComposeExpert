929677c939f0ecd0d5b61783fb94e110e196cc6d
pablo galvan <gvpablodaniel@gmail.com>
2024-10-26 21:43:16 -0300

    EP 10 AnimatedContent

--- LoginSample/app/src/main/java/com/example/loginsample/MainActivity.kt
+++ LoginSample/app/src/main/java/com/example/loginsample/MainActivity.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedContentTransitionScope.SlideDirection.Companion.Up
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.Crossfade
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -27,6 +32,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -141,6 +147,27 @@ fun LoginScreen(
                 Text(text = "Login")
             }
         }
+        //AnimatedContentComponent()
+    }
+}
+
+@Composable
+fun AnimatedContentComponent() {
+    var counter by remember { mutableIntStateOf(0) }
+    Button( onClick =  {
+        counter++
+    }){
+        Text("Increase counter")
+    }
+    AnimatedContent(
+        targetState = counter,
+        transitionSpec = {
+            (slideIntoContainer(Up) + fadeIn())
+                .togetherWith(slideOutOfContainer(Up) + fadeOut())
+        },
+        label = ""
+    ){
+        Text("Click counter $it")
     }
 }
 
