e91080a09d068cfb7c072dbf6ca5c9c73c655f1b
pablo galvan <gvpablodaniel@gmail.com>
2024-10-27 22:55:13 -0300

    EP 11 First instrumental test

--- LoginSample/app/src/androidTest/java/com/example/loginsample/ExampleInstrumentedTest.kt
+++ LoginSample/app/src/androidTest/java/com/example/loginsample/ExampleInstrumentedTest.kt
@@ -1,24 +1,32 @@
 package com.example.loginsample
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-@RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.loginsample", appContext.packageName)
+    fun clickButton(): Unit = with(composeTestRule) {
+        setContent {
+            var text by remember { mutableStateOf("Hello") }
+            Button(onClick = { text = "Bye" }) {
+                Text(text = text)
+            }
+        }
+        onNodeWithText("Hello").performClick()
+        onNodeWithText("Bye").assertExists()
     }
 }
